c6923469645adea5e1fff0f758eadacdf3418fd2
daniellimadev <pdaniel643@gmail.com>
2024-06-13 21:32:49 -0300

    Iniciando o projeto e configuração do banco de dados MySQL

---
+++ src/test/java/com/github/daniellimadev/futebolapi/FutebolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.daniellimadev.futebolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutebolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
